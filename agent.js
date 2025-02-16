b6f6618de8a01e32c0adab39c230af6294e307bf
DarthYvonne <anderscolding@gmail.com>
2025-02-16 13:35:50 +0100

    Update agent.js

--- agent.js
+++ agent.js
@@ -4,7 +4,7 @@ const WebSocket = require("ws");
 const axios = require("axios");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`); 
